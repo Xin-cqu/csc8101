d3ccbfc999cccb5afbc9c9cff6a087d8660b41b5
xwen <xwen@ubuntu.(none)>
2014-02-26 15:12:53 +0000

    3 commit

--- src/main/java/uk/ac/ncl/cs/csc8101/weblogcoursework/test.java
+++ src/main/java/uk/ac/ncl/cs/csc8101/weblogcoursework/test.java
@@ -1,27 +1,27 @@
-package uk.ac.ncl.cs.csc8101.weblogcoursework;
-import com.datastax.driver.core.*;
-
-import java.io.IOException;
-import java.text.ParseException;
-
-/**
- * Created by xwen on 2/5/14.
- */
-public class test {
-    public static void main(String args[]) throws IOException, ParseException {
-//        PartOne p = new PartOne();
-//        p.staticSetup();
-//        p.insert(1,1,"a");
-//        p.insert(1,2,"b");
-//        p.insert(1,3,"c");
-//        p.insert(1,4,"d");
-//        p.showUrl(1,2,3);
-//        p.staticCleanup();
-//        ReadBuffer r=new ReadBuffer();
-//        r.readDataFile();
-        PartTwo p = new PartTwo();
-
-        p.read();
-
-    }
-}
+//package uk.ac.ncl.cs.csc8101.weblogcoursework;
+//import com.datastax.driver.core.*;
+//
+//import java.io.IOException;
+//import java.text.ParseException;
+//
+///**
+// * Created by xwen on 2/5/14.
+// */
+//public class test {
+//    public static void main(String args[]) throws IOException, ParseException {
+////        PartOne p = new PartOne();
+////        p.staticSetup();
+////        p.insert(1,1,"a");
+////        p.insert(1,2,"b");
+////        p.insert(1,3,"c");
+////        p.insert(1,4,"d");
+////        p.showUrl(1,2,3);
+////        p.staticCleanup();
+////        ReadBuffer r=new ReadBuffer();
+////        r.readDataFile();
+//        PartTwo p = new PartTwo();
+//
+//        p.read();
+//
+//    }
+//}
